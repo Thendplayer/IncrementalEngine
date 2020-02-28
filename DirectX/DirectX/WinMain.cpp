765828b176556c811031518cbb65648ad6d11815
PolVega <polvegadiaz@gmail.com>
2020-02-28 12:11:29 +0100

    Second proposition for the base structure, this time using singletone and pointers initialized on declaration of a game class.

--- DirectX/DirectX/WinMain.cpp
+++ DirectX/DirectX/WinMain.cpp
@@ -1,15 +1,15 @@
 #include <windows.h>
 #include <windowsx.h>
 #include "MainEngine.h"
-
 #include "ExampleGame.h"
-using namespace MyGame;
 
+using namespace MyGame;
 using namespace MyEngine;
+
+auto const GAME = new ExampleGame;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	auto a = new ExampleGame;
 	Engine::Get()->Run(hInstance);
 	return 0;
 }
