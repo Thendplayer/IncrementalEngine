28e7ec16ce6a072e19b8286c69344def176caa7a
PolVega <polvegadiaz@gmail.com>
2020-05-13 13:01:32 +0200

    First MVP.

--- IncrementalEngine/IncrementalEngine/ExampleGame.cpp
+++ IncrementalEngine/IncrementalEngine/ExampleGame.cpp
@@ -27,47 +27,5 @@ namespace MyGame
 	void ExampleGame::Update(float dt)
 	{
 		_displayText->SetText(std::to_wstring(_tap->GetValue()));
-
-		auto input = Engine::Get()->GetInput();
-		
-		if (input->IsKeyDown(DirectInputKey::W))
-		{
-			_tap->SetScale(_tap->GetScale().x + 2 * dt, _tap->GetScale().y + 2 * dt);
-		}
-		
-		if (input->IsKeyDown(DirectInputKey::S))
-		{
-			_tap->SetScale(_tap->GetScale().x - 2 * dt, _tap->GetScale().y - 2 * dt);
-		}
-		
-		if (input->IsKeyDown(DirectInputKey::UpArrow))
-		{
-			_tap->SetPosition(_tap->GetPosition().x, _tap->GetPosition().y - 100 * dt);
-		}
-		
-		if (input->IsKeyDown(DirectInputKey::DownArrow))
-		{
-			_tap->SetPosition(_tap->GetPosition().x, _tap->GetPosition().y + 100 * dt);
-		}
-		
-		if (input->IsKeyDown(DirectInputKey::RightArrow))
-		{
-			_tap->SetPosition(_tap->GetPosition().x + 100 * dt, _tap->GetPosition().y);
-		}
-		
-		if (input->IsKeyDown(DirectInputKey::LeftArrow))
-		{
-			_tap->SetPosition(_tap->GetPosition().x - 100 * dt, _tap->GetPosition().y);
-		}
-		
-		if (input->IsKeyDown(DirectInputKey::Space))
-		{
-			_tap->SetRotation(_tap->GetRotation() + 200 * dt);
-		}
-		
-		if (input->IsKeyDown(DirectInputKey::P))
-		{
-			Engine::Get()->GetSoundEngine()->Play(Engine::Get()->GetResources()->GetSound("Points"));
-		}
 	}
 }
