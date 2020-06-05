b26031e07bab44acd046dccff541bb53dba75a6a
PolVega <polvegadiaz@gmail.com>
2020-06-05 18:21:15 +0200

    Resources added.

--- IncrementalGame/Game/UpgradesPerClickPanelData.h
+++ IncrementalGame/Game/UpgradesPerClickPanelData.h
@@ -5,13 +5,10 @@
 
 #define UpgradesPerClickPanelData vector<UpgradeData>\
 		{\
-			UpgradeData(L"Sapphire amulet", Formula(15.f, 1.f, 2), Formula(15.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(100.f, 1.f, 2), Formula(100.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(1100.f, 1.f, 2), Formula(1100.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(12000.f, 1.f, 2), Formula(12000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(130000.f, 1.f, 2), Formula(130000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(1400000.f, 1.f, 2), Formula(1400000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(20000000.f, 1.f, 2), Formula(20000000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(330000000.f, 1.f, 2), Formula(330000000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(5100000000.f, 1.f, 2), Formula(5100000000.f, 1.f, 2))\
+			UpgradeData(L"Wool", Formula(15.f, 1.f, 2), Formula(15.f, 1.f, 2)),\
+			UpgradeData(L"Magic feather", Formula(100.f, 1.f, 2), Formula(100.f, 1.f, 2)),\
+			UpgradeData(L"Pitchfork", Formula(1100.f, 1.f, 2), Formula(1100.f, 1.f, 2)),\
+			UpgradeData(L"Mace", Formula(12000.f, 1.f, 2), Formula(12000.f, 1.f, 2)),\
+			UpgradeData(L"Bomb", Formula(130000.f, 1.f, 2), Formula(130000.f, 1.f, 2)),\
+			UpgradeData(L"Card club", Formula(1400000.f, 1.f, 2), Formula(1400000.f, 1.f, 2))\
 		}
--- IncrementalGame/Game/UpgradesPerSecondPanelData.h
+++ IncrementalGame/Game/UpgradesPerSecondPanelData.h
@@ -5,13 +5,18 @@
 
 #define UpgradesPerSecondPanelData vector<UpgradeData>\
 		{\
-			UpgradeData(L"Sapphire amulet", Formula(15.f, 1.f, 2), Formula(15.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(100.f, 1.f, 2), Formula(100.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(1100.f, 1.f, 2), Formula(1100.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(12000.f, 1.f, 2), Formula(12000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(130000.f, 1.f, 2), Formula(130000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(1400000.f, 1.f, 2), Formula(1400000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(20000000.f, 1.f, 2), Formula(20000000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(330000000.f, 1.f, 2), Formula(330000000.f, 1.f, 2)),\
-			UpgradeData(L"Sapphire amulet", Formula(5100000000.f, 1.f, 2), Formula(5100000000.f, 1.f, 2))\
+			UpgradeData(L"Willow", Formula(15.f, 1.f, 2), Formula(15.f, 1.f, 2)),\
+			UpgradeData(L"Book", Formula(100.f, 1.f, 2), Formula(100.f, 1.f, 2)),\
+			UpgradeData(L"Enchanted willow", Formula(1100.f, 1.f, 2), Formula(1100.f, 1.f, 2)),\
+			UpgradeData(L"Oak wand", Formula(12000.f, 1.f, 2), Formula(12000.f, 1.f, 2)),\
+			UpgradeData(L"Ruby ring", Formula(130000.f, 1.f, 2), Formula(130000.f, 1.f, 2)),\
+			UpgradeData(L"Oak staff", Formula(1400000.f, 1.f, 2), Formula(1400000.f, 1.f, 2)),\
+			UpgradeData(L"Fire spells", Formula(20000000.f, 1.f, 2), Formula(20000000.f, 1.f, 2)),\
+			UpgradeData(L"Fire staff", Formula(330000000.f, 1.f, 2), Formula(330000000.f, 1.f, 2)),\
+			UpgradeData(L"Yellow potion", Formula(5100000000.f, 1.f, 2), Formula(5100000000.f, 1.f, 2)),\
+			UpgradeData(L"Regeneration spells", Formula(10100000000.f, 1.f, 2), Formula(10100000000.f, 1.f, 2)),\
+			UpgradeData(L"Skull", Formula(63300000000.f, 1.f, 2), Formula(63300000000.f, 1.f, 2)),\
+			UpgradeData(L"Dark spells", Formula(120000000000.f, 1.f, 2), Formula(120000000000.f, 1.f, 2)),\
+			UpgradeData(L"Green potion", Formula(1300000000000.f, 1.f, 2), Formula(120000000000.f, 1.f, 2)),\
+			UpgradeData(L"Sapphire amulet", Formula(8200000000000.f, 1.f, 2), Formula(120000000000.f, 1.f, 2))\
 		}
